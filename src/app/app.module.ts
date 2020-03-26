a151939a39587a907a8a935e00cf8fbd0117e8b4
Renato Mertz <renatomertz@gmail.com>
2020-03-26 09:34:49 -0300

    Fixing import HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import {
   MatInputModule,
   MatPaginatorModule,
@@ -43,7 +45,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule ],
+    MatFormFieldModule,
+    HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
